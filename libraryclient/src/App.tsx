8d120b8bea270d256ccc8574723307b5f048af0b
peytonbarre <peypeyozzy@gmail.com>
2023-11-28 23:48:35 -0600

    Check discord for changes

--- libraryclient/src/App.tsx
+++ libraryclient/src/App.tsx
@@ -5,28 +5,22 @@ import { Home } from "./pages/Home";
 import { Fines } from "./pages/Fines";
 import { Borrower } from "./pages/Borrower";
 import { Search } from "./pages/Search";
-import { Landing } from "./pages/Landing";
 import { Navbar } from "./components/Navbar";
-import { LandingNav } from "./components/LandingNav";
-import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 
 function App() {
   const location = useLocation();
 
-  const isLandingPage = location.pathname === "/landing" 
-  const isLoginPage = location.pathname === "/login" || location.pathname === "/signup"
-
   return (
     <div>
-      {/* Might need to validate this logic lol */}
-      {isLandingPage ? <LandingNav/> : isLoginPage ? <></> : <Navbar/>}
+      <Navbar/>
       <Container>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/fines" element={<Fines />} />
           <Route path="/borrower" element={<Borrower />} />
           <Route path="/search" element={<Search />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </Container>
     </div>
